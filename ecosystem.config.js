b49a73dc93f7f047240b66e17b3c498d09ae98f4
Tone Lopes Monte <tonelopes.dev@gmail.com>
2025-01-13 19:01:26 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,27 @@ module.exports = {
     {
       name: "api",
       script: "./src/server.js",
-      instances: 1,
+      instances: "max",
+      exec_mode: "cluster",
+      watch: false,
+      increment_var: "PORT",
+      autorestart: true,
+      max_memory_restart: "500M",
       env: {
         NODE_ENV: "development",
+        PORT: 3333,
       },
       env_production: {
         NODE_ENV: "production",
+        PORT: 3001,
       },
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      log_file: "logs/app.log",
+      error_file: "logs/err.log",
+      out_file: "logs/out.log",
+
+      merge_logs: true,
+      time: true,
     },
   ],
 };
